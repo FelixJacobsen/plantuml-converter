74afd04e67937ec8fc8aac92a978025f40a192de
FelixJacobsen <felix.jacobsen@iths.se>
2023-05-20 02:07:27 +0200

    Initial commit

---
+++ src/main/java/feljac/tech/plantumlconverter/PlantumlConverterApplication.java
@@ -0,0 +1,13 @@
+package feljac.tech.plantumlconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantumlConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantumlConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/feljac/tech/plantumlconverter/PlantumlConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package feljac.tech.plantumlconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantumlConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
